9e962dd48bce41465339973d7d9f94a299711264
Shahules786 <Shahules786@gmail.com>
2023-04-18 12:46:57 +0530

    add custom trainer

--- blade2blade/training/trainer.py
+++ blade2blade/training/trainer.py
@@ -10,6 +10,20 @@ from blade2blade.training.custom_datasets.utils import get_dataset
 from blade2blade.training.utils import get_model, get_tokenizer
 
 
+class BladeTrainer(Trainer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+
+        if not model.config.is_encoder_decoder:
+            _ = inputs.pop("decoder_attention_mask")
+
+        outputs = model(**inputs)
+        loss = outputs.get("loss")
+        return (loss, outputs) if return_outputs else loss
+
+
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def train(cfg: DictConfig) -> None:
     if not os.path.exists(cfg.log_dir):
@@ -44,7 +58,7 @@ def train(cfg: DictConfig) -> None:
     )
 
     # Initialize our Trainer
-    trainer = Trainer(
+    trainer = BladeTrainer(
         model=model,
         args=training_args,
         train_dataset=train_dataset,
